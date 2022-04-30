5f361aafff49d483caa3da2983913c936a73c04f
JP Etcheber <jetcheber@gmail.com>
2022-04-30 12:27:50 -0700

    use keypress motion

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from pyglet.window import key
 from tickytacky.screen import Screen
 from tickytacky.sprite import Sprite
 from tickytacky.clock import Clock
@@ -13,11 +14,47 @@ def init():
     main_screen.add_sprite("snail", [10, 10])
 
 
-def update(dt):
+def snail(new_loc=False):
     old_loc = main_screen.sprites["snail"]["location"]
-    new_loc = [old_loc[0] + 1, old_loc[1]]
-    main_screen.add_sprite("snail", new_loc)
+    if new_loc:
+        move_snail = [old_loc[0] + new_loc[0], old_loc[1] + new_loc[1]]
+        main_screen.add_sprite("snail", move_snail)
+    else:
+        main_screen.add_sprite("snail", old_loc)
+
+
+def update(dt):
+    snail()
     pass
+
+
+def up():
+    snail([0, -1])
+
+
+def down():
+    snail([0, 1])
+
+
+def left():
+    snail([-1, 0])
+
+
+def right():
+    snail([1, 0])
+
+
+@main_screen.event
+def on_text_motion(motion):
+    arrow_keys = {
+        key.MOTION_UP: up,
+        key.MOTION_DOWN: down,
+        key.MOTION_LEFT: left,
+        key.MOTION_RIGHT: right
+    }
+    arrow_keys[motion]()
+
+
 
 
 if __name__ == "__main__":
